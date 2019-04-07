2d27a5e21e732cac67c99e063b1bd89b047d9a95
AllenI <eradinus@yahoo.com>
2019-04-07 11:40:36 -0700

    Add test3 multiple sketches

--- TEST3.m
+++ TEST3.m
@@ -8,10 +8,13 @@ L1_ = [1,.25];
 V1_ = [0.0552,-2.3906];
 U1_ = [-.2086,-.2401];
 G1_ = [.086,-1.5542];
-beta = -20*d2r;
+beta = [10,5,0,-5,-10]*d2r;
 [alpha,gamma,alpha_,gamma_] = stephenson3Disp_InitConfig_Crank(W1,V1,U1,G1,L1,V1_,U1_,G1_,beta);
-Chain = [rotVec2D(W1,beta);...
-    rotVec2D(L1,alpha);...
-    rotVec2D(V1_,alpha_);...
-    -rotVec2D(U1_,gamma_)];
-drawKinematicChain2D(Chain, [0,0])
+for i = 1:length(beta)
+    Chain = [rotVec2D(W1,beta(i));...
+    rotVec2D(L1,alpha(i));...
+    rotVec2D(V1_,alpha_(i));...
+    -rotVec2D(U1_,gamma_(i))];
+    drawKinematicChain2D(Chain, [0,0]);
+    hold on;
+end
